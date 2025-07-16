6e510da1a779c8c6ed80fc64af59c9892d676cfa
liamr03 <liamr03@hotmail.com>
2025-07-16 14:18:40 +0200

    Initial commit

---
+++ src/main/java/org/example/phmonitor/PhMonitorApplication.java
@@ -0,0 +1,13 @@
+package org.example.phmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/phmonitor/PhMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.phmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
